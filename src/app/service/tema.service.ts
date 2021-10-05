8bcdec7f2cae0bfb1d850cda5b0b36c5b1464e42
EsterGNN <gomes.ester23@gmail.com>
2021-10-05 11:58:07 -0300

    Implementação do edit e delete do Tema

--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -21,7 +21,19 @@ export class TemaService {
     return this.http.get<Tema[]>('https://blogestergomes.herokuapp.com/tema', this.token)
   }
 
+  getByIdTema(id: number): Observable<Tema> {
+    return this.http.get<Tema>(`https://blogestergomes.herokuapp.com/tema/${id}`, this.token)
+  }
+
   postTema(tema: Tema): Observable<Tema> {
     return this.http.post<Tema>('https://blogestergomes.herokuapp.com/tema', tema, this.token)
+  }
+
+  putTema(tema: Tema): Observable<Tema> {
+    return this.http.put<Tema>('https://blogestergomes.herokuapp.com/tema', tema, this.token)
+  }
+
+  deleteTema(id: number) {
+    return this.http.delete(`https://blogestergomes.herokuapp.com/tema/${id}`, this.token)
   }
 }
